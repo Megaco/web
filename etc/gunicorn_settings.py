bbbdf0780bd4ba37e4ecb55de6bb14fcb681e4fc
Ilya Gudkov <rad.shuhart@gmail.com>
2016-03-03 21:25:30 +0200

    changed gunicorn settings

--- etc/gunicorn_settings.py
+++ etc/gunicorn_settings.py
@@ -2,7 +2,7 @@ import multiprocessing
 
 bind = "0.0.0.0:8000"
 workers = multiprocessing.cpu_count() * 2 + 1
-daemon = True
+daemon = False
 pidfile='gunicorn_pid'
-accesslog='gunicorn_access.log'
-errorlog='gunicorn_error.log'
+# accesslog='gunicorn_access.log'
+# errorlog='gunicorn_error.log'
--- etc/init.sh
+++ etc/init.sh
@@ -8,7 +8,7 @@ sudo /etc/init.d/nginx restart
 #source bin/activate
 #cd ../
 
-sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 
 cd /home/box/web
 virtualenv --python=python3 --always-copy venv
@@ -19,7 +19,8 @@ pip install Django
 #django-admin startproject ask
 #gunicorn hello:application
 
-gunicorn -c /home/box/web/etc/gunicorn_settings.py /home/box/web/ask/ask/wsgi.py
-
+#gunicorn -c /home/box/web/etc/gunicorn_settings.py /home/box/web/ask/ask/wsgi.py
+#cd ask
+gunicorn ask.wsgi:application --pythonpath ask
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
