434288e2391dbace75d69e2fc50246c1ff1d32a0
Ilya Gudkov <rad.shuhart@gmail.com>
2016-03-03 21:05:58 +0200

    added django project

---
+++ ask/qa/urls.py
@@ -0,0 +1,13 @@
+__author__ = 'ozzy'
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^signup/', views.index, name='signup'),
+    url(r'^question/([^/]+)/', views.test, name='question'),
+    url(r'^ask/', views.index, name='ask'),
+    url(r'^popular/', views.index, name='popular'),
+    url(r'^new/', views.index, name='new'),
+    url(r'^$', views.test,name='index')
+]
--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get install -y nginx
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
@@ -12,8 +13,13 @@ sudo ln -s /home/box/etc/hello.py /etc/gunicorn.d/hello.py
 cd /home/box/web
 virtualenv --python=python3 --always-copy venv
 source venv/bin/activate
+
 pip install gunicorn
+pip install Django
+#django-admin startproject ask
 #gunicorn hello:application
-gunicorn -c /home/box/web/etc/hello.py  hello:application
+
+gunicorn -c /home/box/web/etc/gunicorn_settings.py /home/box/web/ask/ask/wsgi.py
+
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
